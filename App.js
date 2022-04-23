94529a38e4cfd95a6c23847476aa848741aa0d59
Wellington Cidade <welltecnic@hotmail.com>
2022-04-23 14:01:35 -0300

    Projeto Login

--- App.js
+++ App.js
@@ -1,10 +1,38 @@
-
-import { StyleSheet, Text, View } from 'react-native';
+import{ Button,Input,Text} from 'react-native-elements';
+import { StyleSheet, View } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World !!</Text>
+      <Text h2>Entre com seu Login</Text>
+
+      <Input 
+        placeholder="Digite seu usuario"
+        leftIcon={{type:'font-awesome',name:'user'}}
+        onChangeText={value => Text(value)}
+
+      />
+      <Input
+        placeholder="Digite sua senha"
+        leftIcon={{type:'font-awesome',name:'lock'}}
+        onChangeText={value =>setPassword (value)}
+        secureTextEntry={true}
+      />
+      <Button
+        icon={
+          <Icon
+            name="check"
+            size={20}
+            color="white"
+          />
+        }
+        title="Logar"
+        onPress={()=> entrar()}
+
+      />
+
+
     </View>
   );
 }
